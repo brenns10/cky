e15e06478cfd2dbf259c9d271cb9ac568b3fdec9
Stephen Brennan <smb196@case.edu>
2015-05-22 16:36:24 -0400

    Fixed the coverage script to make clean the coverage before making.

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,12 @@
-# Run test coverage
+# This script cleans up the old test coverage and re-runs it.
+
+# Clean coverage!
+make clean_cov
+# Compile the coverage version again.
 make CFG=coverage
+# Run the tests of the coverage build.
 bin/coverage/main -t
+# Create the HTML coverage site.
 make gcov
+# Open it!
 xdg-open cov/index.html
