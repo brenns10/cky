a35b47aa2edf7593b22114033b8b6ce66f4f4c92
Travis Builder <smb196@case.edu>
2015-05-24 22:30:08 +0000

    [ci skip] Autodoc commit for 0ad93502db832e8fad48371420a574fab769504b.

--- doc/html/search/all_67.js
+++ doc/html/search/all_67.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['get_5fescape',['get_escape',['../str_8c.html#a2812a14f008e19005dec1ae9f31d0a93',1,'get_escape(const wchar_t **source, wchar_t epsilon):&#160;str.c'],['../str_8h.html#a2812a14f008e19005dec1ae9f31d0a93',1,'get_escape(const wchar_t **source, wchar_t epsilon):&#160;str.c']]],
+  ['get_5fescape',['get_escape',['../str_8c.html#a40d05018488beaaf81d524413833a682',1,'get_escape(const wchar_t **source):&#160;str.c'],['../str_8h.html#a40d05018488beaaf81d524413833a682',1,'get_escape(const wchar_t **source):&#160;str.c']]],
   ['gram_2ec',['gram.c',['../gram_8c.html',1,'']]],
   ['gram_2eh',['gram.h',['../gram_8h.html',1,'']]]
 ];
--- doc/html/search/functions_67.js
+++ doc/html/search/functions_67.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['get_5fescape',['get_escape',['../str_8c.html#a2812a14f008e19005dec1ae9f31d0a93',1,'get_escape(const wchar_t **source, wchar_t epsilon):&#160;str.c'],['../str_8h.html#a2812a14f008e19005dec1ae9f31d0a93',1,'get_escape(const wchar_t **source, wchar_t epsilon):&#160;str.c']]]
+  ['get_5fescape',['get_escape',['../str_8c.html#a40d05018488beaaf81d524413833a682',1,'get_escape(const wchar_t **source):&#160;str.c'],['../str_8h.html#a40d05018488beaaf81d524413833a682',1,'get_escape(const wchar_t **source):&#160;str.c']]]
 ];
