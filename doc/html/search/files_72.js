088658b8b312b4e2f156e7e37bf6e9465796e721
Travis Builder <smb196@case.edu>
2015-05-25 03:39:42 +0000

    [ci skip] Autodoc commit for 5b708ffa399cfff134c4f8479d25af0b95538201.

--- doc/html/search/files_72.js
+++ doc/html/search/files_72.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['regex_2eh',['regex.h',['../regex_8h.html',1,'']]],
+  ['regexsearchtest_2ec',['regexsearchtest.c',['../regexsearchtest_8c.html',1,'']]],
   ['regextest_2ec',['regextest.c',['../regextest_8c.html',1,'']]]
 ];
