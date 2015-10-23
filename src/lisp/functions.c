c4b14f7872fdb4edecfca87b2d00f8d1e47cefe0
Stephen Brennan <smb196@case.edu>
2015-10-23 09:48:50 -0400

    Added builtins for subtraction and length!

--- src/lisp/functions.c
+++ src/lisp/functions.c
@@ -13,18 +13,42 @@
 
 *******************************************************************************/
 
+#include <wchar.h>
+
 #include "libstephen/ht.h"
 #include "lisp.h"
 
+unsigned int wchar_hash(DATA data)
+{
+  wchar_t *wc = data.data_ptr;
+  unsigned int hash = 0;
+
+  while (wc && *wc != L'\0') {
+    hash = (hash << 5) - hash + *wc;
+    wc++;
+  }
+
+  return hash;
+}
+
+int data_compare_wstring(DATA d1, DATA d2)
+{
+  wchar_t *ws1, *ws2;
+  ws1 = d1.data_ptr;
+  ws2 = d2.data_ptr;
+  return wcscmp(ws1, ws2);
+}
+
 /**
-   @brief Add two values.
+   @brief Add any number of values.
  */
 static lisp_value *lisp_add(lisp_list *params)
 {
   lisp_int *rv = (lisp_int*)tp_int.tp_alloc();
   while (params) {
     if (params->value->type != &tp_int) {
-      fprintf(stderr, "lisp_add(): error\n");
+      fprintf(stderr, "lisp_add(): type error\n");
+      exit(EXIT_FAILURE);
     }
     rv->value += ((lisp_int*)params->value)->value;
     params = params->next;
@@ -32,6 +56,70 @@ static lisp_value *lisp_add(lisp_list *params)
   return (lisp_value *)rv;
 }
 
+/**
+   @brief Return the length of a list.
+ */
+static lisp_value *lisp_length(lisp_list *params)
+{
+  lisp_int *i;
+  if (params == NULL) {
+    fprintf(stderr, "lisp_length(): too few arguments\n");
+    exit(EXIT_FAILURE);
+  }
+  if (params->value->type != &tp_list) {
+    fprintf(stderr, "lisp_length(): incorrect type argument\n");
+    exit(EXIT_FAILURE);
+  }
+
+  i = (lisp_int*)tp_int.tp_alloc();
+  i->value = lisp_list_length((lisp_list*)params->value);
+  return (lisp_value*)i;
+}
+
+/**
+   @brief Subtract some number of values form the first one.
+ */
+static lisp_value *lisp_subtract(lisp_list *params)
+{
+  lisp_int *rv;
+  int len = lisp_list_length(params);
+
+  if (len == 0) {
+    fprintf(stderr, "lisp_subtract(): too few arguments\n");
+    exit(EXIT_FAILURE);
+  } else if (len == 1) {
+    // negate the argument
+    if (params->value->type != &tp_int) {
+      fprintf(stderr, "lisp_subtract(): wrong type argument\n");
+      exit(EXIT_FAILURE);
+    }
+    rv = (lisp_int*)tp_int.tp_alloc();
+    rv->value = -((lisp_int*)params->value)->value;
+  } else {
+    if (params->value->type != &tp_int) {
+      fprintf(stderr, "lisp_subtract(): wrong type argument\n");
+      exit(EXIT_FAILURE);
+    }
+    rv = (lisp_int*)tp_int.tp_alloc();
+    rv->value = ((lisp_int*)params->value)->value;
+
+    params = params->next;
+    while (params) {
+      if (params->value->type != &tp_int) {
+        fprintf(stderr, "lisp_subtract(): wrong type argument\n");
+        exit(EXIT_FAILURE);
+      }
+      rv->value -= ((lisp_int*)params->value)->value;
+      params = params->next;
+    }
+  }
+
+  return (lisp_value*)rv;
+}
+
+/**
+   @brief
+ */
 
 /**
    @brief Return a scope containing the top-level variables for our lisp.
@@ -41,11 +129,19 @@ lisp_scope *lisp_create_globals(void)
   lisp_scope *scope = smb_new(lisp_scope, 1);
   lisp_builtin *bi;
   scope->up = NULL;
-  ht_init(&scope->table, &ht_string_hash, &data_compare_string);
+  ht_init(&scope->table, &wchar_hash, &data_compare_wstring);
 
   bi = (lisp_builtin*)tp_builtin.tp_alloc();
   bi->function = &lisp_add;
-  ht_insert(&scope->table, PTR("+"), PTR(bi));
+  ht_insert(&scope->table, PTR(L"+"), PTR(bi));
+
+  bi = (lisp_builtin*)tp_builtin.tp_alloc();
+  bi->function = &lisp_subtract;
+  ht_insert(&scope->table, PTR(L"-"), PTR(bi));
+
+  bi = (lisp_builtin*)tp_builtin.tp_alloc();
+  bi->function = &lisp_length;
+  ht_insert(&scope->table, PTR(L"length"), PTR(bi));
 
   return scope;
 }
